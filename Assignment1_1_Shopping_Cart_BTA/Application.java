548966827959d86ae970ab3eb141b4e52f7be1ac
Ben Albright <btalbr01@gmail.com>
2023-08-31 19:25:34 -0400

    fourth commit

--- Assignment1_1_Shopping_Cart_BTA/Application.java
+++ Assignment1_1_Shopping_Cart_BTA/Application.java
@@ -2,7 +2,24 @@
 public class Application {
 
 	public static void main(String[] args) {
-		ShoppingCart aCart = new ShoppingCart();
-
+		Item pen = new Item("Pen", 199);
+		Item potatoes = new Item("Potatoes", 599);
+		Item wrench = new Item("Wrench", 899);
+		
+		ShoppingCart cart = new ShoppingCart(5);
+		
+		cart.addItem(pen);
+		cart.addItem(potatoes);
+		cart.addItem(wrench);
+		
+		System.out.println("There are " + cart.countItems() + " items in the cart");
+		
+		cart.cartContents();
+		System.out.println("Total cost is: " + cart.getTotalCost());
+		
+		cart.remove(pen);
+		cart.cartContents();
+		
+		
 	}//end main
 }//end class
--- Assignment1_1_Shopping_Cart_BTA/ShoppingCart.java
+++ Assignment1_1_Shopping_Cart_BTA/ShoppingCart.java
@@ -3,42 +3,126 @@ public class ShoppingCart<T>
 {
 	int totalPrice;
 	int itemCount;
-	String itemName;
-	int itemCost;
-	int quantity;
-	BagInterface<Item> shoppingCart = new ArrayBag<Item>();
-	private T[] bag; // Cannot be final due to doubling
+	Item[] cart;
+	int capacity;
+	int storedItem;
+	private int numberOfEntries;
+	private boolean integrityOK = false;
 	
-	public ShoppingCart(int index, T value)
+	public ShoppingCart(int capacity)
 	{
-		this.bag[index] = value;
+		cart = new Item[capacity];
+		integrityOK = true;
 	} // end constructor
 	
 	public ShoppingCart()
 	{
-		this.bag = (T[]) new Object[10];
+		cart = new Item[5];
 	}
 	
-	public void addItem(ShoppingCart merch)
+	public boolean isEmpty()
 	{
-		this.quantity = this.quantity + merch.quantity;
+      return numberOfEntries == 0;
+	} // end isEmpty
+	
+	public void addItem(Item input)
+	{
+		if(storedItem < cart.length)
+		{
+			cart[storedItem] = input;
+			storedItem++;
+		}
+		else System.out.println("The shopping cart is full");
 	}
 	
-	public void removeItem(ShoppingCart merch)
+	public Item removeItem()
 	{
-		this.quantity = this.quantity - merch.quantity;
+		checkintegrity();
+		Item result = removeEntry(numberOfEntries - 1);
+	      return result;
 	}
 	
-	public int getTotalCost(int totalCost)
+	public boolean remove(Item anEntry)
 	{
-		return totalCost;
+		checkintegrity();
+      int index = getIndexOf(anEntry);
+      Item result = removeEntry(index);
+      return anEntry.equals(result);
+	} // end remove
+	
+	private Item removeEntry(int givenIndex)
+	{
+		Item result = null;
+      
+		if (!isEmpty() && (givenIndex >= 0))
+		{
+         result = cart[givenIndex];          // Entry to remove
+         int lastIndex = numberOfEntries - 1;
+         cart[givenIndex] = cart[lastIndex];  // Replace entry to remove with last entry
+         cart[lastIndex] = null;             // Remove reference to last entry
+         numberOfEntries--;
+		} // end if
+      
+      return result;
+	} // end removeEntry
+	
+	public String getTotalCost()
+	{
+		for(int i = 0; i < cart.length; i++)
+		{
+			if(cart[i] != null)
+			totalPrice = totalPrice + cart[i].getPrice();
+		}
+		return "$" + totalPrice/100 + "." + totalPrice%100;	
 	}
 	
-	public int countItems(int itemCount)
+	public int countItems()
 	{
+		itemCount = 0;
+		for(int i = 0; i < cart.length; i++)
+		{
+			if(cart[i] != null)
+			itemCount++;
+		} 	
 		return itemCount;
 	}
 	
+	public void cartContents()
+	{
+		for(int i = 0; i < cart.length; i++)
+		{
+			if(cart[i] != null)
+			System.out.println(cart[i]);
+	}}
+	
+	private int getIndexOf(Item anEntry)
+	{
+		int where = -1;
+		boolean found = false;
+		int index = 0;
+      
+      while (!found && (index < numberOfEntries))
+		{
+			if (anEntry.equals(cart[index]))
+			{
+				found = true;
+				where = index;
+			} // end if
+         index++;
+		} // end while
+	
+	// Assertion: If where > -1, anEntry is in the array bag, and it
+    // equals bag[where]; otherwise, anEntry is not in the array.
+    
+		return where;
+	} // end getIndexOf
+	
+	   // Throws an exception if receiving object is not initialized.
+	   private void checkintegrity()
+	   {
+	      if (!integrityOK)
+	         throw new SecurityException ("ArrayBag object is corrupt.");
+	   } // end checkintegrity
 	
 	
 }//end ShoppingCart
